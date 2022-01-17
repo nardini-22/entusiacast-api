4fc044f0148244474a416e95430889da6835f9c0
ArSaN <arthnardini24@gmail.com>
2022-01-17 14:11:50 -0300

    Bugfix/Added-error-handling

--- app.js
+++ app.js
@@ -8,17 +8,46 @@ const EpisodesRoutes = require("./src/routes/index");
 
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Accept, Authorization, Content-Type"
+  );
+  ("");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Method", "GET, POST, PUT, DELETE");
+    return res.status(200).send({});
+  }
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 app.use(express.json());
 
-app.use("/v1", EpisodesRoutes);
-
 app.get("/", (req, res, next) => {
   res.send({
     message: "Bem-vindo",
+  });
+});
+
+app.use("/v1", EpisodesRoutes);
+
+app.use((req, res, next) => {
+  const erro = new Error("Rota não encontrada!");
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      message: error.message,
+    },
   });
 });
 
